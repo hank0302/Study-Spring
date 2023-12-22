cf5b7d93416e3ffdb38c8c950c0e0e9761f6472e
hank0302 <kangh1596@gmail.com>
2023-12-22 22:11:30 +0900

    [2023_12_22]- Spring 예외처리학습

---
+++ exception/src/test/java/han/study/exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package han.study.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
