588b70424eabc3455cbbec5bdaabb318ef7e6afa
hank0302 <kangh1596@gmail.com>
2024-01-03 14:01:43 +0900

    [2023_12_26]- Transaction, TX전파 공부

---
+++ transaction/src/test/java/han/study/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package han.study.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
