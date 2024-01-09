606c55e6c8c94f2cbe9d7b6b0b2847f478e7c87d
hank0302 <kangh1596@gmail.com>
2024-01-09 17:48:06 +0900

    [2023_01_09]- AOP관련 여러 실습 진행

---
+++ aop/src/main/java/han/study/aop/exam/ExamService.java
@@ -0,0 +1,16 @@
+package han.study.aop.exam;
+
+import han.study.aop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId){
+        examRepository.save(itemId);
+    }
+}
